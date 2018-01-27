eb8a0987479078c2f0a29a9d43dd359bb2acbf04
qiuyu <530004000@qq.com>
2018-01-27 18:34:29 +0800

    测试

---
+++ src/demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiuyu
+ * Date: 2018/1/27
+ * Time: 下午6:32
+ */
+
+namespace Qiuyuhome;
+
+class Demo
+{
+    public function index()
+    {
+        dump('This is a demo for packagist.org');
+        return true;
+    }
+}
